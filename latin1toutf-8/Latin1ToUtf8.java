c8a21d0b22302ba391f0ec4968db99a9f96cfe32
Ari Makela <ari.makela@iki.fi>
2023-04-18 12:38:52 +0300

    read data from file

--- latin1toutf-8/Latin1ToUtf8.java
+++ latin1toutf-8/Latin1ToUtf8.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Latin1ToUtf8 {
+    static final String FILE = "skandeja.txt";
+
     public static void main(String[] args) {
-        System.out.println("hello, world");
+        try {
+            File file = new File(FILE);
+            Scanner reader = new Scanner(file);
+
+            while(reader.hasNextLine()) {
+                String line = reader.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.println("file not found");
+            ex.printStackTrace();
+        }
     }
 }
