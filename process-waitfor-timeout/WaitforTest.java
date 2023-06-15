f86b3feaf17ec6baae225fd0d2dbf24d8b41a427
Ari Makela <ari.makela@iki.fi>
2023-06-15 21:27:04 +0300

    run a process

--- process-waitfor-timeout/WaitforTest.java
+++ process-waitfor-timeout/WaitforTest.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.lang.Process;
+import java.lang.ProcessBuilder;
+
 class WaitforTest {
     public static void main(String[] args) {
-        System.out.println("hello, world");
+        final ProcessBuilder processBuilder = new ProcessBuilder("./sleep-5.sh");
+        try {
+            final Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+        } catch(IOException ex) {
+            System.out.println(ex);
+        } catch(InterruptedException ex) {
+            System.out.println(ex);
+        }
     }
 }
