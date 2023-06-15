63b6a72ed1623a03a7974cb176e759fdbf7a9595
Ari Makela <ari.makela@iki.fi>
2023-06-15 21:34:33 +0300

    use timeout

--- process-waitfor-timeout/WaitforTest.java
+++ process-waitfor-timeout/WaitforTest.java
@@ -1,13 +1,17 @@
 import java.io.IOException;
 import java.lang.Process;
 import java.lang.ProcessBuilder;
+import java.util.concurrent.TimeUnit;
 
 class WaitforTest {
     public static void main(String[] args) {
         final ProcessBuilder processBuilder = new ProcessBuilder("./sleep-5.sh");
+
         try {
             final Process process = processBuilder.start();
-            int exitCode = process.waitFor();
+            boolean exitCode = process.waitFor(2, TimeUnit.SECONDS);
+            process.destroyForcibly();
+            System.out.println("exitCode=" + exitCode);
         } catch(IOException ex) {
             System.out.println(ex);
         } catch(InterruptedException ex) {
